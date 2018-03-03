01e85b5198b6c1693849a953c1864f53511ca1d6
eggyducktective <anhkaehne@gmail.com>
2018-03-03 13:52:00 +1100

    create game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-console.log("Up and running!");
+console.log('Up and running!');
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log(cardOne);
+alert('user flipped queen.');
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log(cardTwo);
+alert('user flipped king.');
+
+var numberOfCards = cardsInPlay.length;
+
+if (numberOfCards === 2) {
+	if (cardsInPlay[0] === cardsInPlay[3]) {
+		alert('You found a match');
+	} else {
+		alert('Sorry, try again');
+	}
+}
