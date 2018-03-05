9b3fa9dfa4418eaa303cef4962e8aded0a5815fd
eggyducktective <anhkaehne@gmail.com>
2018-03-05 23:25:51 +1100

    fixed error

--- js/main.js
+++ js/main.js
@@ -1,35 +1,24 @@
 console.log('Up and running!');
 
 
-
-
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-if (numberOfCards === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match");
+	var numberOfCards = cardsInPlay.length;
+	if (numberOfCards === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
 	} else {
-		console.log("Sorry, try again");
+		alert("Sorry, try again");
 	}
 };
 
 
 var flipCard = function (cardID) {
-console.log("User flipped " + cards[cardID]);
-cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
 };
-
-
-var numberOfCards = cardsInPlay.length;
-if (numberOfCards === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match");
-} else {
-	alert("Sorry, try again");
-}
-
-
 
 flipCard(0);
 flipCard(2);
